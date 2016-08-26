997e07c0b320061038291d038a989386e88c949c
Jakob Rosenberg <jakobrosenberg@gmail.com>
2016-08-26 14:30:54 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ declare module MeteorValidatedMethod {
     export class ValidatedMethod<T> {
         constructor(options: ValidatedMethodOptions<T>);
         call(options?: T, cb?: (err, res)=> void): void;
+        run(options?: T, cb?: (err, res)=> void): void;
     }
 
     interface ValidatedMethodOptions<T> {
